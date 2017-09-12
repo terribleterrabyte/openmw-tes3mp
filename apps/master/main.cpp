d1388cdf84041cb476130d17fe02da8dec2cdf96
Koncord <stas5978@gmail.com>
2017-09-12 20:56:21 +0800

    [Master] Add Admin Rest Server
    Add admin rest callback - "OnAdminRequest". Called on every POST request

--- apps/master/main.cpp
+++ apps/master/main.cpp
@@ -4,12 +4,14 @@
 #include <extern/sol/sol.hpp>
 #include "MasterServer.hpp"
 #include "RestServer.hpp"
+#include "AdminRest.hpp"
 
 using namespace RakNet;
 using namespace std;
 
 unique_ptr<RestServer> restServer;
 shared_ptr<MasterServer> masterServer;
+unique_ptr<AdminRest> restAdminServer;
 
 int main(int argc, char* argv[])
 {
@@ -28,10 +30,30 @@ int main(int argc, char* argv[])
 
 
         restServer = make_unique<RestServer>(restPort.as<unsigned short>(), masterServer->GetServers());
+
+        sol::object restAdminCert = config["restAdminCert"];
+        if (restAdminCert.get_type() != sol::type::string)
+            throw runtime_error("config.restAdminCert is not correct");
+
+        sol::object restAdminKey = config["restAdminKey"];
+        if (restAdminKey.get_type() != sol::type::string)
+            throw runtime_error("config.restAdminKey is not correct");
+
+        sol::object restAdminVerifyFile = config["restAdminVerifyFile"];
+        if (restAdminVerifyFile.get_type() != sol::type::string)
+            throw runtime_error("config.restAdminVerifyFile is not correct");
+
+        sol::object restAdminPort = config["restAdminPort"];
+        if (restAdminPort.get_type() != sol::type::number)
+            throw runtime_error("config.restAdminPort is not correct");
+
+        restAdminServer = make_unique<AdminRest>(restAdminCert.as<string>(), restAdminKey.as<string>(),
+                                                 restAdminVerifyFile.as<string>(), restAdminPort.as<unsigned short>(), masterServer);
     });
 
     auto onExit = [](int /*sig*/){
         restServer->stop();
+        restAdminServer->stop();
         masterServer->luaStuff([](sol::state &state) {
             sol::protected_function func = state["OnExit"];
             if (func.valid())
@@ -47,8 +69,10 @@ int main(int argc, char* argv[])
     masterServer->Start();
 
     thread rest_thread([]() { restServer->start();});
+    thread restAdmin_thread([]() { restAdminServer->start();});
 
     rest_thread.join();
+    restAdmin_thread.join();
     masterServer->Wait();
 
     return 0;
