4e93905350af5fe362299b156aa39cac63048e5c
Koncord <stas5978@gmail.com>
2017-09-12 20:21:32 +0800

    [Master] Backport SimpleWeb

--- apps/master/SimpleWeb/https_server.hpp
+++ apps/master/SimpleWeb/https_server.hpp
@@ -1,91 +1,82 @@
-#ifndef HTTPS_SERVER_HPP
-#define HTTPS_SERVER_HPP
+#pragma once
 
 #include "base_server.hpp"
+
+#ifdef USE_STANDALONE_ASIO
+#include <asio/ssl.hpp>
+#else
 #include <boost/asio/ssl.hpp>
-#include <openssl/ssl.h>
+#endif
+
 #include <algorithm>
+#include <openssl/ssl.h>
 
-namespace SimpleWeb
-{
-    typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket> HTTPS;
+namespace SimpleWeb {
+    using HTTPS = asio::ssl::stream<asio::ip::tcp::socket>;
 
-    template<>
-    class Server<HTTPS> : public ServerBase<HTTPS>
-    {
+    template <>
+    class Server<HTTPS> : public ServerBase<HTTPS> {
         std::string session_id_context;
         bool set_session_id_context = false;
-    public:
-        Server(const std::string &cert_file, const std::string &private_key_file,
-               const std::string &verify_file = std::string()) : ServerBase<HTTPS>::ServerBase(443),
-                                                                 context(boost::asio::ssl::context::tlsv12)
-        {
-            context.use_certificate_chain_file(cert_file);
-            context.use_private_key_file(private_key_file, boost::asio::ssl::context::pem);
 
-            if (verify_file.size() > 0)
-            {
+    public:
+        Server(const std::string &cert_file, const std::string &private_key_file, const std::string &verify_file = std::string())
+                : ServerBase<HTTPS>::ServerBase(443), context(asio::ssl::context::tlsv12) {
+            context.use_certificate_chain_file(cert_file);
+            context.use_private_key_file(private_key_file, asio::ssl::context::pem);
+
+            if(verify_file.size() > 0) {
                 context.load_verify_file(verify_file);
-                context.set_verify_mode(boost::asio::ssl::verify_peer | boost::asio::ssl::verify_fail_if_no_peer_cert |
-                                        boost::asio::ssl::verify_client_once);
+                context.set_verify_mode(asio::ssl::verify_peer | asio::ssl::verify_fail_if_no_peer_cert | asio::ssl::verify_client_once);
                 set_session_id_context = true;
             }
         }
 
-        void start()
-        {
-            if (set_session_id_context)
-            {
+        void start() override {
+            if(set_session_id_context) {
                 // Creating session_id_context from address:port but reversed due to small SSL_MAX_SSL_SESSION_ID_LENGTH
                 session_id_context = std::to_string(config.port) + ':';
                 session_id_context.append(config.address.rbegin(), config.address.rend());
-                SSL_CTX_set_session_id_context(context.native_handle(),
-                                               reinterpret_cast<const unsigned char *>(session_id_context.data()),
-                                               std::min<size_t>(session_id_context.size(),
-                                                                SSL_MAX_SSL_SESSION_ID_LENGTH));
+                SSL_CTX_set_session_id_context(context.native_handle(), reinterpret_cast<const unsigned char *>(session_id_context.data()),
+                                               std::min<size_t>(session_id_context.size(), SSL_MAX_SSL_SESSION_ID_LENGTH));
             }
             ServerBase::start();
         }
 
     protected:
-        boost::asio::ssl::context context;
+        asio::ssl::context context;
 
-        virtual void accept()
-        {
-            //Create new socket for this connection
-            //Shared_ptr is used to pass temporary objects to the asynchronous functions
-            auto socket = std::make_shared<HTTPS>(*io_service, context);
+        void accept() override {
+            auto session = std::make_shared<Session>(create_connection(*io_service, context));
 
-            acceptor->async_accept((*socket).lowest_layer(), [this, socket](const boost::system::error_code &ec)
-            {
-                //Immediately start accepting a new connection (if io_service hasn't been stopped)
-                if (ec != boost::asio::error::operation_aborted)
-                    accept();
+            acceptor->async_accept(session->connection->socket->lowest_layer(), [this, session](const error_code &ec) {
+                auto lock = session->connection->handler_runner->continue_lock();
+                if(!lock)
+                    return;
 
+                if(ec != asio::error::operation_aborted)
+                    this->accept();
 
-                if (!ec)
-                {
-                    boost::asio::ip::tcp::no_delay option(true);
-                    socket->lowest_layer().set_option(option);
+                if(!ec) {
+                    asio::ip::tcp::no_delay option(true);
+                    error_code ec;
+                    session->connection->socket->lowest_layer().set_option(option, ec);
 
-                    //Set timeout on the following boost::asio::ssl::stream::async_handshake
-                    auto timer = get_timeout_timer(socket, config.timeout_request);
-                    socket->async_handshake(boost::asio::ssl::stream_base::server, [this, socket, timer]
-                            (const boost::system::error_code &ec)
-                    {
-                        if (timer)
-                            timer->cancel();
-                        if (!ec)
-                            read_request_and_content(socket);
-                        else if (on_error)
-                            on_error(std::shared_ptr<Request>(new Request(*socket)), ec);
+                    session->connection->set_timeout(config.timeout_request);
+                    session->connection->socket->async_handshake(asio::ssl::stream_base::server, [this, session](const error_code &ec) {
+                        session->connection->cancel_timeout();
+                        auto lock = session->connection->handler_runner->continue_lock();
+                        if(!lock)
+                            return;
+                        if(!ec)
+                            this->read_request_and_content(session);
+                        else if(this->on_error)
+                            this->on_error(session->request, ec);
                     });
                 }
-                else if (on_error)
-                    on_error(std::shared_ptr<Request>(new Request(*socket)), ec);
+                else if(this->on_error)
+                    this->on_error(session->request, ec);
             });
         }
     };
-}
-
-#endif //HTTPS_SERVER_HPP
+} // namespace SimpleWeb
